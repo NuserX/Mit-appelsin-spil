fff2c4e9fb37f9bf21a92c6ea0dfc59e338e19e5
Nuser <OHAgmund@gmail.com>
2021-09-02 11:28:35 -0700

    Første forsøg

---
+++ sketch.js
@@ -0,0 +1,367 @@
+
+
+// Appelsinen
+let x = 0; 
+let y = 500;
+const rad = 20;
+let xspeed = 4;
+let yspeed = -10;
+let newspeed;
+const grav = 0.1;
+
+// Image
+let imgPlayerone;
+let imgPlayertwo;
+
+let heart;
+let damage;
+
+    // Overworld
+    let imgBackground;
+    let pie;
+    let apple;
+    let cake;
+    let bat;
+    let fireball;
+    let egg;
+    let arrow;
+    let carrot;
+    
+    // Cave 
+    let imgBackgroundCave;
+    let coal;
+    let flint
+    let copper;
+    let iron;
+    let redstone;
+    let lapiz;
+    
+
+    // Obsidian
+    let imgBackroundObsidian;
+    let emerald;
+    let gold;
+    let diamond;
+    let obsidian;
+
+    //Nether
+    let imgBackgroundNether;
+
+
+    let bucket;
+    let pickaxe;
+    // Øvrige
+
+let victor;
+let missed = 0;
+let tid = 50;
+let tidTæller = 40 + tid + Math.random() * tid;
+
+let spilIgang = true;
+let ranNum;
+
+let msgCounter = 0;
+
+let obsidianCounter = 0;
+
+
+// Player objects
+let playerOne;
+let playerTwo;
+
+let playerOneLives = 5;
+let playerTwoLives = 5;
+
+
+let scorePlayerOne = 0;
+let scorePlayerTwo = 0;
+
+let playerIronCounter = 0;
+let playerDiamondCounter = 0;
+let playerObsidianCounter = 0;
+
+let playerOneBucket = false;
+let playerTwoBucket = false;
+
+let playerOnePickaxe = false;
+let playerTwoPickaxe = false;
+
+
+
+
+function preload() {
+    // Player
+    imgPlayerone = loadImage('assets/player/play1.png');
+    imgPlayertwo = loadImage('assets/player/play2.png');
+
+    heart = loadImage('assets/heart.png');
+    damage = loadImage('assets/damage.png');
+
+    // Overworld
+    imgBackground = loadImage('assets/overworld/background.png');
+    pie = loadImage('assets/overworld/pie.png');
+    apple = loadImage('assets/overworld/apple.png');
+    cake = loadImage('assets/overworld/cake.png');
+    egg = loadImage('assets/overworld/egg.png');
+    carrot = loadImage('assets/overworld/carrot.png');
+
+
+    // Cave 
+    imgBackgroundCave = loadImage('assets/cave/backgroundCave.png');
+    imgBackgroundDeepCave = loadImage('assets/cave/deepCave.jpg');
+    coal = loadImage('assets/cave/coal.png');
+    flint = loadImage('assets/cave/flint.png');
+    copper = loadImage('assets/cave/copper.png');
+    iron = loadImage('assets/cave/iron.png');
+    redstone = loadImage('assets/cave/redstone.png');
+    lapiz = loadImage('assets/cave/lapiz.png');
+
+    arrow = loadImage('assets/cave/arrow.png');
+    bat = loadImage('assets/cave/bat.png');
+
+    array1 = [coal, flint, copper, iron];
+
+
+    // Obsidian
+    imgBackroundObsidian = loadImage('assets/obsidian/obsidian.jpg');
+    emerald = loadImage('assets/obsidian/emerald.png');
+    gold = loadImage('assets/obsidian/gold.png');
+    diamond = loadImage('assets/obsidian/diamond.png');
+    obsidian = loadImage('assets/obsidian/obsidian.png');
+
+
+    bucket = loadImage('assets/bucket.png');
+    pickaxe = loadImage('assets/overworld/apple.png');
+
+}
+
+// Sounds
+const backgroundSound = new Audio();
+backgroundSound.src = "assets/sounds/minecraftTheme.mp3";
+
+const chickenDeathSound1 = new Audio();
+chickenDeathSound1.src = "assets/sounds/chicken1.mp3";
+
+const chickenWalkingSound1 = new Audio();
+chickenWalkingSound1.src = "assets/sounds/chicken3.mp3";
+
+const rabbitDeathSound1 = new Audio();
+rabbitDeathSound1.src = "assets/sounds/rabbit3.mp3";
+
+const rabbitWalkingSound = new Audio();
+rabbitWalkingSound.src = "assets/sounds/rabbit4.mp3";
+
+
+function setup() {  
+    createCanvas(1200, 600);
+
+    textAlign(CENTER, CENTER);
+
+    newspeed = yspeed;
+    x = rad;
+
+    playerOne = new Kurv(330, 380, 150, 50, 15, imgPlayerone);
+    playerTwo = new Kurv(850, 400, 150, 50, 15, imgPlayertwo);
+   
+    
+}
+
+
+
+function draw() {
+
+    if (spilIgang == true) {
+        backgroundSound.play();
+        cutscene();
+        
+        if (msgCounter == 1 || msgCounter == 3 || msgCounter == 5 || msgCounter == 7) {
+            customFucntion();
+            checkPlayerLives();
+            move();
+            checkScore();
+            display(); 
+        }
+            
+        if (playerOne !=null) {
+            if (keyIsDown(87)) {
+                playerOne.moveY(-8);
+            }
+            if (keyIsDown(83)) {
+                playerOne.moveY(8);
+            }    
+            if (keyIsDown(65)) {
+                playerOne.moveX(-8);
+            }
+            if (keyIsDown(68)) {
+                playerOne.moveX(8);
+            } 
+        }
+        if (playerTwo != null) {
+            if (keyIsDown(UP_ARROW)) {
+                playerTwo.moveY(-8);
+            }
+            if (keyIsDown(DOWN_ARROW)) {
+                playerTwo.moveY(8);
+            }    
+            if (keyIsDown(LEFT_ARROW)) {
+                playerTwo.moveX(-8);
+            }
+            if (keyIsDown(RIGHT_ARROW)) {
+                playerTwo.moveX(8);
+            } 
+        }
+        playerOneLivesChange();
+        playerTwoLivesChange();
+    } else {  
+        fill(255);
+        textSize(46);
+        text("Game Over",width/2, height/2);
+        text("Winner: " + victor);
+    }
+    
+}
+
+
+
+
+function display() {
+    fill(0);
+    textSize(20);
+    text("Player One:", width-1130,30);
+    text("Player Two:", width-229,30);
+    
+    if (tidTæller <= 100) {
+        appelsinerImages();
+    } else {
+        tidTæller -= 1;
+    }
+     
+    if (playerOne != null) {
+        playerOne.tegn();
+    }
+    if (playerTwo != null) {
+        playerTwo.tegn();
+    }
+}
+    
+
+
+function move() {
+    if (tidTæller <= 100) {
+        x += xspeed;    
+        y += yspeed;
+        yspeed += grav;
+    }
+    
+    if (x > width || y > height) {
+        shootNew();
+        missed += 1;
+    }
+    
+}
+
+
+function checkScore() {
+    if (yspeed > 0) {
+        if (playerOne != null) {
+            if (playerOne.grebet(x, y, rad)) {
+                playerOnePoints();
+                shootNew(); 
+            } 
+        }
+        
+        if (playerTwo != null) {
+            if (playerTwo.grebet(x,y,rad)) {
+                playerTwoPoints();
+                shootNew();
+            }
+        }
+        if (playerOne == null && (playerTwo == null)) {
+            spilIgang= false;
+        }
+    } 
+}
+
+
+function shootNew() {
+    x = rad;
+    y = random(0, 633);
+
+    if (y < 100) {
+        yspeed = 0;
+        xspeed = random(5,11);
+    } else if (y < 300 && y > 101) {
+        yspeed = -5;
+        xspeed = random (3,8);
+    } else if (y < 500 && y > 301) {
+        yspeed = -7.5;
+        xspeed = random (8,10);
+    } else {
+        yspeed = -10;
+        xspeed = random(1,8);
+    }
+
+    
+    ranNum = int(random(1,5));
+
+    if (playerOnePickaxe == true && playerTwoPickaxe == true) {
+        ranNum = 23;
+    } else if (playerOne == null && playerTwoPickaxe == true) {
+        ranNum = 23;
+    } else if (playerTwo == null && playerOnePickaxe == true) {
+        ranNum = 23;
+    }else if (playerDiamondCounter == 3) {
+        ranNum = 99;
+        playerDiamondCounter = 0;
+    } else if (playerOneBucket == true && playerTwoBucket == true) {
+        ranNum = int(random(13,22));
+    } else if (scorePlayerOne >= 10 && scorePlayerTwo >= 10) {
+        ranNum = int(random(5,13));
+        if (playerIronCounter == '3') {
+            ranNum = 100;
+            playerIronCounter = 0;
+        } 
+    } 
+    
+    tidTæller = 20 + tid + Math.random() * tid;
+    
+}
+
+function keyPressed() {
+    if (spilIgang == false && keyCode == 82) {
+        restart();
+    } 
+}
+
+
+function restart() {
+    x = 0; 
+    y = 500;
+    xspeed = 4;
+    yspeed = -10;
+    
+    missed = 0;
+    tid = 50;
+    tidTæller = 40 + tid + Math.random() * tid;
+   
+    playerOneLives = 5;
+    playerTwoLives = 5;
+
+    scorePlayerOne = 0;
+    scorePlayerTwo = 0;
+
+    playerOneIron = 0;
+    playerTwoIron = 0;
+
+    playerOneDiamond = 0;
+    playerTwoDiamond = 0;
+
+    playerOneBucket = false;
+    playerTwoBucket = false;
+
+    ranNum = 0;
+    spilIgang = true;
+
+    playerOne = new Kurv(330, 380, 150, 50, 15, imgPlayerone);
+    playerTwo = new Kurv(850, 400, 150, 50, 15, imgPlayertwo);
+}
---
+++ switch.js
@@ -0,0 +1,367 @@
+// Cutscenes
+function cutscene() {
+    if (msgCounter === 0) {
+        fill(0);
+        textSize(46);
+        text("Objective: Reach the nether",width/2, height/3);
+        setTimeout(() => {
+            text("Task 1: Gather food to enter a cave",width/2, height/2); 
+            setTimeout(() => {
+                msgCounter = 1; 
+            }, 2000);
+        }, 2000);
+    } else if (msgCounter === 2) {
+        setTimeout(() => {
+            fill(255);
+            textSize(46);
+            text("Task 2: Gather enough iron to make a bucket",width/2, height/2); 
+            setTimeout(() => {
+                msgCounter = 3; 
+            }, 4000);
+        }, 1000);
+    } else if (msgCounter === 4) {
+        setTimeout(() => {
+            fill(255);
+            textSize(46);
+            text("Task 3: Gather diamonds to make a Pickaxe",width/2, height/2);
+            setTimeout(() => {
+                msgCounter = 5; 
+            }, 4000);
+        }, 1000);
+    } else if (msgCounter === 6) {
+        setTimeout(() => {
+            fill(255);
+            textSize(46);
+            text("Task 4: Mine enough obsidian to make a portal",width/2, height/2);
+            setTimeout(() => {
+                msgCounter = 7; 
+            }, 4000);
+        }, 1000);
+    } else if (msgCounter === 8) {
+        setTimeout(() => {
+            fill(255);
+            textSize(46);
+            text("You've reached the nether!",width/2, height/2);
+            setTimeout(() => {
+                msgCounter = 9; 
+            }, 4000);
+        }, 1000);
+    }
+}
+
+
+// Change background function
+function customFucntion() {            
+
+        if (playerObsidianCounter >= 14) {
+            background(0)
+            if (msgCounter == 7) {
+                msgCounter = 8;
+            }
+        } else if (playerOne == null && playerTwoPickaxe == true) {
+            background(imgBackroundObsidian);
+            if (msgCounter == 5) {
+                msgCounter = 6;
+            }
+        } else if (playerTwo == null && playerOnePickaxe == true) {
+            background(imgBackroundObsidian);
+            if (msgCounter == 5) {
+                msgCounter = 6;
+            }
+        } else if (playerOnePickaxe == true && playerTwoPickaxe == true) {
+            background(imgBackroundObsidian);
+            if (msgCounter == 5) {
+                msgCounter = 6;
+            }
+        } else if (playerOne == null && playerTwoBucket == true) {
+            background(imgBackgroundDeepCave);
+            if (msgCounter == 3) {
+                msgCounter = 4;
+            }
+        } else if (playerTwo == null && playerOneBucket == true) {
+            background(imgBackgroundDeepCave);
+            if (msgCounter == 3) {
+                msgCounter = 4;
+            }
+        } else if (playerOneBucket == true && playerTwoBucket == true) {
+            background(imgBackgroundDeepCave);
+            if (msgCounter == 3) {
+                msgCounter = 4;
+            }
+        } else if (scorePlayerOne >= 10 && scorePlayerTwo >= 10) {
+            background(imgBackgroundCave);
+            if (msgCounter == 1) {
+                msgCounter = 2;
+            }
+        } else {
+            background(imgBackground);
+        }
+
+    
+}
+
+// Check if player is dead
+function checkPlayerLives() {
+    if (playerOneLives <= 0 ) {
+        playerOne = null;
+    } 
+    if (playerTwoLives <= 0){
+        playerTwo = null;
+    }
+}
+
+
+// First player life-meter changes
+function playerOneLivesChange() {
+    switch (playerOneLives) {
+        case 5:
+            image(heart, width-1070,15,30,30);
+            image(heart, width-1039,15,30,30);
+            image(heart, width-1008,15,30,30);
+            image(heart, width-977,15,30,30);
+            image(heart, width-946,15,30,30);
+            break;
+        case 4:
+            image(heart, width-1070,15,30,30);
+            image(heart, width-1039,15,30,30);
+            image(heart, width-1008,15,30,30);
+            image(heart, width-977,15,30,30);
+            image(damage, width-946,15,30,30);
+            break;
+        case 3:
+            image(heart, width-1070,15,30,30);
+            image(heart, width-1039,15,30,30);
+            image(heart, width-1008,15,30,30);
+            image(damage, width-977,15,30,30);
+            image(damage, width-946,15,30,30);
+            break;
+        case 2:
+            image(heart, width-1070,15,30,30);
+            image(heart, width-1039,15,30,30);
+            image(damage, width-1008,15,30,30);
+            image(damage, width-977,15,30,30);
+            image(damage, width-946,15,30,30);
+            break;
+        case 1:
+            image(heart, width-1070,15,30,30);
+            image(damage, width-1039,15,30,30);
+            image(damage, width-1008,15,30,30);
+            image(damage, width-977,15,30,30);
+            image(damage, width-946,15,30,30);
+            break;
+        default:
+            if (playerOneLives <= 0) {
+                image(damage, width-1070,15,30,30);
+                image(damage, width-1039,15,30,30);
+                image(damage, width-1008,15,30,30);
+                image(damage, width-977,15,30,30);
+                image(damage, width-946,15,30,30);
+            } else {
+                text("There was an error", width-820,30);
+            }
+            break;
+    }
+}
+
+// Second player life-meter changes
+function playerTwoLivesChange() {
+    switch (playerTwoLives) {
+        case 5:
+            image(heart, width-169,15,30,30);
+            image(heart, width-138,15,30,30);
+            image(heart, width-107,15,30,30);
+            image(heart, width-76,15,30,30);
+            image(heart, width-45,15,30,30);
+            break;
+        case 4:
+            image(heart, width-169,15,30,30);
+            image(heart, width-138,15,30,30);
+            image(heart, width-107,15,30,30);
+            image(heart, width-76,15,30,30);
+            image(damage, width-45,15,30,30);
+            break;
+        case 3:
+            image(heart, width-169,15,30,30);
+            image(heart, width-138,15,30,30);
+            image(heart, width-107,15,30,30);
+            image(damage, width-76,15,30,30);
+            image(damage, width-45,15,30,30);
+            break;
+        case 2:
+            image(heart, width-169,15,30,30);
+            image(heart, width-138,15,30,30);
+            image(damage, width-107,15,30,30);
+            image(damage, width-76,15,30,30);
+            image(damage, width-45,15,30,30);
+            break;
+        case 1:
+            image(heart, width-169,15,30,30);
+            image(damage, width-138,15,30,30);
+            image(damage, width-107,15,30,30);
+            image(damage, width-76,15,30,30);
+            image(damage, width-45,15,30,30);
+            break;
+        default:
+            if (playerTwoLives <= 0) {
+                image(damage, width-169,15,30,30);
+                image(damage, width-138,15,30,30);
+                image(damage, width-107,15,30,30);
+                image(damage, width-76,15,30,30);
+                image(damage, width-45,15,30,30);
+            } else {
+                text("There was an error", width-320,30);
+            }
+            break;
+    }
+}
+
+// Different oranges skins
+function appelsinerImages() {
+    switch (ranNum) {
+        case 1:
+            image(apple, x, y, 60, 50);
+            break;
+        case 2:
+            image(pie, x,  y, 60, 50);
+            break;
+        case 3:
+            image(egg, x, y, 60, 50);
+            break;
+        case 4:
+            image(carrot, x, y, 60, 50);
+            break;
+
+        case 5:
+            image(coal, x, y, 60, 50);
+            break;
+        case 6:
+            image(flint, x, y, 60, 50);
+            break;
+        case 7:
+            image(copper, x, y, 60, 50);
+            break;
+        case 8:
+            image(iron, x, y, 60, 50);
+            break;
+        case 9:
+        case 10:
+            image(bat, x, y, 60, 50);
+            break;
+        case 11:
+        case 12:
+            image(arrow, x, y, 60, 50);
+            break;
+
+        case 13:
+        case 14:
+            image(redstone, x, y, 60, 50);
+            break;
+        case 15:
+        case 16:
+            image(lapiz, x, y, 60, 50);
+            break;
+        case 17:
+        case 18:
+            image(gold, x, y, 60, 50);              
+            break;
+        case 19:
+        case 20:
+            image(emerald, x, y, 60, 50);
+            break;
+        case 21:
+        case 22:
+            image(diamond, x, y, 60, 50);
+            break;
+        case 23:
+            image(obsidian, x, y, 60, 50);
+            break;
+        case 99: 
+            image(pickaxe, x, y, 60, 50);
+            break;
+        case 100:
+            image(bucket, x, y, 60, 50);
+            break;
+        default:
+            image(cake, x, y, 60, 50);
+            break;
+    }
+
+}
+
+// First player points and lives regulator
+function playerOnePoints() {
+    switch (ranNum) {
+        case 8:
+            chickenWalkingSound1.play();
+            playerIronCounter += 1;
+            break;
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+            chickenDeathSound1.play();
+            playerOneLives -= 1;
+            break;
+        
+        case 21:
+        case 22:
+            chickenWalkingSound1.play();
+            playerDiamondCounter += 1;
+            break;
+        case 23:
+        case 24:
+        case 25:
+            chickenWalkingSound1.play();
+            playerObsidianCounter += 1;
+            break;
+        case 99:
+            chickenWalkingSound1.play();
+            playerOnePickaxe = true;
+            break;
+        case 100:
+            playerOneBucket = true;
+            break;        
+        default:
+            chickenWalkingSound1.play();
+            scorePlayerOne += 1;
+            break;
+    }
+}
+
+// Second player points and lives regulator
+function playerTwoPoints() {
+    switch (ranNum) {
+        case 8:
+            rabbitWalkingSound.play();
+            playerIronCounter += 1;
+            break;
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+            rabbitDeathSound1.play();
+            playerTwoLives -= 1;
+            break;
+        case 21:
+        case 22:
+            rabbitWalkingSound.play();
+            playerDiamondCounter += 1;
+            break;
+        case 23:
+        case 24:
+        case 25:
+            rabbitWalkingSound.play();
+            playerObsidianCounter += 1;
+        case 99:
+            rabbitWalkingSound.play();
+            playerTwoPickaxe = true;
+            break;
+        case 100:
+            playerTwoBucket = true;
+            break
+        default:
+            rabbitWalkingSound.play();
+            scorePlayerTwo += 1;
+            break;
+    }
+}
